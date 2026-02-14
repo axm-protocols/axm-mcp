90510527925eab93e1cbc0a0354c0ce1b6b31c1f
JarryGabriel <jarry.gabriel@gmail.com>
2026-02-14 23:46:11 +0100

    chore: stop tracking auto-generated _version.py

--- src/axm_mcp/_version.py
+++ src/axm_mcp/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev8+ga50da0952.d20260214'
-__version_tuple__ = version_tuple = (0, 1, 'dev8', 'ga50da0952.d20260214')
+__version__ = version = '0.1.dev12+g340a1e15b.d20260214'
+__version_tuple__ = version_tuple = (0, 1, 'dev12', 'g340a1e15b.d20260214')
 
 __commit_id__ = commit_id = None
